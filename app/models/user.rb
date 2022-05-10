f6dbf9a2ff99d9655672519b24794a22c1f3ae4c
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-05-10 15:45:25 -0400

    Seeded content with users, messages, and conversations created from messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many: :conversations
-    has_many: :messages
-    has_many: :received_messages, class_name: "Conversation", foreign_key: "recipient_id"
-    has_many: :sent_messages, class_name: "Conversation", foreign_key: "sender_id" 
+    has_many :conversations
+    has_many :messages
+    has_many :received_messages, class_name: "Conversation", foreign_key: "recipient_id"
+    has_many :sent_messages, class_name: "Conversation", foreign_key: "sender_id" 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create 3 users
+user1 = User.create(username: "Bobby")
+user2 = User.create(username: "Sally")
+user3 = User.create(username: "Minty")
+
+
+message1 = Message.create(user: user1, conversation: Conversation.create(sender: user1, recipient: user2), content: "First Message")
+message2 = Message.create(user: user1, conversation: Conversation.create(sender: user1, recipient: user2), content: "Second Message")
+message3 = Message.create(user_id: user1, conversation_id: Conversation.create(sender_id: user1, recipient_id: user3), content: "Third Message")
+message4 = Message.create(user: user1, conversation: Conversation.create(sender: user1, recipient: user3), content: "Fourth Message")
+message5 = Message.create(user: user1, conversation: Conversation.create(sender: user2, recipient: user1), content: "Reply")
+
