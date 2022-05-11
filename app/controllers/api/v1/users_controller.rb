911a54d82a896f030c59a1d205571abb5c68771b
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-05-11 09:51:14 -0400

    Users_Controller create method filled out - finds a username if it exist or create the username if it doesn't

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,28 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def new
+        user = User.new(user_params)
+        render json: user
+    end
+
+    def create
+    #   if a user is found log in as that user 
+    # if the user is not create the new user 
+    
+    if User.find_by(username: user_params[:username])
+        user = User.find_by(username: user_params[:username])
+        render json: user
+    else 
+        user = User.create(user_params)
+        user.save 
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.requre(:user).permit(:username)
+    end
 end
