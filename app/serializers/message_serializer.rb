4cb12e79a441885c301f3f3887e0b83e578bb54a
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-05-24 10:06:38 -0400

    Adding to Readme file

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,6 +1,6 @@
 class MessageSerializer 
   include FastJsonapi::ObjectSerializer
-  attributes  :id, :user, :conversation, :content
-  belongs_to :user
+  attributes  :id, :user_id, :conversation_id, :content
+  belongs_to :user 
   belongs_to :conversation
 end
