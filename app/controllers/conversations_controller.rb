b98def4d6f3ac2921a7500ab28adb9786aa40679
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-05-24 09:05:28 -0400

    Added comments to Conversations Controller Create Method on how it uses the scope method between

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,6 +9,14 @@ class ConversationsController < ApplicationController
      render json: ConversationSerializer.new(conversation)
     end
 
+
+    #lets say the user of the app has id 5 and is trying to start a conversation with 
+    #a user with id 6, tbe betweeb method is called. In this case 5 is the sender_id 
+    #and 6 is the recipient_id. The scope method checks to see if a conversation exist
+    # with 5 as the sender and 6 as the receiver but it also checks if a conversation exist
+    # with 6 being sender and 5 being reciever. making sure they two users will 
+    #have one conversation created
+
     def create
         if Conversation.between(conversation_params[:sender_id],conversation_params[:recipient_id])
             .present?
